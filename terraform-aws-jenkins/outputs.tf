8e4bf89c49aef73deeecbbcf7b07e7466cb9cb11
Mike <63439730+astraeus-sirius@users.noreply.github.com>
2022-07-06 19:08:30 -0400

    Create outputs.tf

---
+++ terraform-aws-jenkins/outputs.tf
@@ -0,0 +1,27 @@
+#------------------------------------------------------------------------------
+# Jenkins Master ALB DNS
+#------------------------------------------------------------------------------
+output "jenkins_master_alb_id" {
+  description = "Jenkins Master Application Load Balancer ID"
+  value       = module.ecs-fargate-service.aws_lb_lb_id
+}
+
+output "jenkins_master_alb_arn" {
+  description = "Jenkins Master Application Load Balancer ARN"
+  value       = module.ecs-fargate-service.aws_lb_lb_arn
+}
+
+output "jenkins_master_alb_arn_suffix" {
+  description = "Jenkins Master Application Load Balancer ARN Suffix"
+  value       = module.ecs-fargate-service.aws_lb_lb_arn_suffix
+}
+
+output "jenkins_master_alb_dns_name" {
+  description = "Jenkins Master Application Load Balancer DNS Name"
+  value       = module.ecs-fargate-service.aws_lb_lb_dns_name
+}
+
+output "jenkins_master_alb_zone_id" {
+  description = "Jenkins Master Application Load Balancer Zone ID"
+  value       = module.ecs-fargate-service.aws_lb_lb_zone_id
+}
